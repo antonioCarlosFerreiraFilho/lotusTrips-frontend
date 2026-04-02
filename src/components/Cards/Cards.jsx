a70ae004dc74436039dee8292fcea34663e1533c
antonioCarlosFerreiraFilho <antonio.carlos.ferreira.filho.99@gmail.com>
2026-04-02 14:24:29 +0100

    Text description

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -14,7 +14,7 @@ const Cards = () => {
           <aside>
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora
-              commodi reiciendis quam itaque,{" "}
+              commodi reiciend loremis quam itaque,{" "}
               <strong>magni corrupti quidem cum</strong>, nam nulla minima,
               porro reprehenderit beatae suscipit dolores sit. Nihil dicta
               eligendi pariatur!
