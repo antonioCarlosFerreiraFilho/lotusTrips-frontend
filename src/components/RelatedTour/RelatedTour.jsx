9b6b8c79022369a85639d49ad42e35d45651b277
antonioCarlosFerreiraFilho <antonio.carlos.ferreira.filho.99@gmail.com>
2026-04-12 18:19:33 +0100

    Style Tours

---
+++ src/components/RelatedTour/RelatedTour.jsx
@@ -0,0 +1,42 @@
+import "./RelatedTour.css";
+
+const RelatedTour = () => {
+  return (
+    <section className="RelatedTour">
+      <article>
+        <aside>
+          <figure>
+            <img src="./Icons/plane-ticket.png" alt="AeroPlane, Avião" />
+          </figure>
+          <h1>Consult With An Expert</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            illo dolore saepe quaerat ratione explicabo tempore ducimus
+          </p>
+        </aside>
+        <aside>
+          <figure>
+            <img src="./Icons/globe.png" alt="Planeta, Planet" />
+          </figure>
+          <h1>Consult With An Expert</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            illo dolore saepe quaerat ratione explicabo tempore ducimus
+          </p>
+        </aside>
+        <aside>
+          <figure>
+            <img src="./Icons/travelling.png" alt="AeroPlane, Avião" />
+          </figure>
+          <h1>Consult With An Expert</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam
+            illo dolore saepe quaerat ratione explicabo tempore ducimus
+          </p>
+        </aside>
+      </article>
+    </section>
+  );
+};
+
+export default RelatedTour;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@
 import "./Home.css";
 //Components
 import BannerSlider from "../../components/BannerSlider/BannerSlider";
+import RelatedTour from "../../components/RelatedTour/RelatedTour";
 import Cards from "../../components/Cards/Cards";
 import CallTour from "../../components/CallTour/CallTour";
 
@@ -9,6 +10,7 @@ const Home = () => {
   return (
     <section>
       <BannerSlider />
+      <RelatedTour />
       <Cards />
       <CallTour />
     </section>
